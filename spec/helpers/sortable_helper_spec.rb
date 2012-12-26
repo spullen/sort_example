97e0b708a56ac3936e3cf6bee860cb647d5224fe
Scott Pullen <s.pullen05@gmail.com>
2012-12-25 23:12:29 -0500

    adding mock view

--- spec/helpers/sortable_helper_spec.rb
+++ spec/helpers/sortable_helper_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 
 describe SortableHelper do
+
+  class MockView < ActionView::Base
+    include ApplicationHelper
+    include SortableHelper
+  end
   
   before(:each) do
-    @helper = Object.new.extend(SortableHelper)
-    @helper.stub!(:sort_column).and_return("column")
-    @helper.stub!(:sort_direction).and_return("asc")
+    @mock_view = MockView.new
+    @mock_view.stub!(:sort_column).and_return("column")
+    @mock_view.stub!(:sort_direction).and_return("asc")
   end
   
   describe '#sortable' do
     context 'given the column is the only specified parameter' do
       before(:each) do
-        @node = Capybara::Node::Simple.new(@helper.sortable("test"))
+        @node = Capybara::Node::Simple.new(@mock_view.sortable("test"))
       end
       
       it 'should set the title to be the capitalized version of the column name' do
