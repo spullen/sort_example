e3695246d10e886895d64e2369395b48b1d6ee9e
Scott Pullen <s.pullen05@gmail.com>
2012-12-25 22:18:42 -0500

    adding sortable helper module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def sortable(column, title = nil, html_options = {})
-    title ||= column.titleize
-    css_class = (column == sort_column) ? "sortable-current sortable-#{sort_direction}" : nil
-    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}.merge(html_options)
-  end
 end
